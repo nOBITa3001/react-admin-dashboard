620e1302e6e875bf6b257c6f46b7abc2eced2a5c
Pongsakorn Thanopassakul <iampossible3001@gmail.com>
2023-05-09 20:06:16 +0200

    feat: add topbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, Theme, ThemeProvider } from "@mui/material";
+import { Topbar } from "./scenes/global/Topbar";
+import { ColorModeContext, useMode } from "./theme";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -10,7 +11,9 @@ function App() {
       <ThemeProvider theme={theme as Theme}>
         <CssBaseline />
         <div className="app">
-          <main className="content"></main>
+          <main className="content">
+            <Topbar />
+          </main>
         </div>
       </ThemeProvider>
     </ColorModeContext.Provider>
