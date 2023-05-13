98b5db4185f8bd9f450eab53bda37483dec9b68d
Pongsakorn Thanopassakul <iampossible3001@gmail.com>
2023-05-13 12:49:38 +0200

    feat: add header

---
+++ src/components/Header.tsx
@@ -0,0 +1,28 @@
+import { Box, Typography, useTheme } from "@mui/material";
+import { tokens } from "../theme";
+
+type Props = {
+  title: string;
+  subtitle: string;
+};
+
+export const Header = ({ title, subtitle }: Props) => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  return (
+    <Box mb={2}>
+      <Typography
+        variant="h2"
+        color={colors.grey[100]}
+        fontWeight="bold"
+        sx={{ mb: "0.25em" }}
+      >
+        {title}
+      </Typography>
+      <Typography variant="h5" color={colors.greenAccent[400]}>
+        {subtitle}
+      </Typography>
+    </Box>
+  );
+};
--- src/scenes/dashboard/index.tsx
+++ src/scenes/dashboard/index.tsx
@@ -1,3 +1,11 @@
+import { Box } from "@mui/material";
+import { Header } from "../../components/Header";
+
 export const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <Box m={2}>
+      <Header title="DASHBOARD" subtitle="all info in once!"></Header>
+      <div>Dashboard</div>
+    </Box>
+  );
 };
